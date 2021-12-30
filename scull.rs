7ef273cc09df9aa84bdb4fb7ad5218cfecf7ded3
Daniel Xu <dxu@dxuuu.xyz>
2021-12-29 19:27:39 -0800

    Rename stuff in source file

--- scull.rs
+++ scull.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 
-//! Rust out-of-tree sample
+//! LDD3 chapter 3 scull module reimplemented in rust
 
 #![no_std]
 #![feature(allocator_api, global_asm)]
@@ -8,30 +8,30 @@
 use kernel::prelude::*;
 
 module! {
-    type: RustOutOfTree,
-    name: b"rust_out_of_tree",
-    author: b"Rust for Linux Contributors",
-    description: b"Rust out-of-tree sample",
+    type: Scull,
+    name: b"scull",
+    author: b"Daniel Xu",
+    description: b"LDD3 chapter 3 scull module",
     license: b"GPL v2",
 }
 
-struct RustOutOfTree {
+struct Scull {
     message: String,
 }
 
-impl KernelModule for RustOutOfTree {
+impl KernelModule for Scull {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
-        pr_info!("Rust out-of-tree sample (init)\n");
+        pr_info!("Rust scull init\n");
 
-        Ok(RustOutOfTree {
+        Ok(Scull {
             message: "on the heap!".try_to_owned()?,
         })
     }
 }
 
-impl Drop for RustOutOfTree {
+impl Drop for Scull {
     fn drop(&mut self) {
         pr_info!("My message is {}\n", self.message);
-        pr_info!("Rust out-of-tree sample (exit)\n");
+        pr_info!("Rust scull exit\n");
     }
 }
